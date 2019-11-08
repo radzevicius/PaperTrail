20d424ba9006ccc676f1c70d393cfd3aabd44df3
Tadas Radzevicius <tadas.radzevicius@tradzevicius-pro.local>
2019-11-09 01:00:05 +0200

    Initial commit

---
+++ src/main/java/com/xDish/PaperTrail/PaperTrailApplication.java
@@ -0,0 +1,13 @@
+package com.xDish.PaperTrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperTrailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperTrailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xDish/PaperTrail/PaperTrailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xDish.PaperTrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperTrailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

c5452604ecfe2e9838ca974aa6ee1348d7f16c3e
Tadas Radzevicius <tadas.radzevicius@tradzevicius-pro.lan>
2019-11-12 11:39:50 +0200

    Playing with response entities

--- src/main/java/com/xDish/PaperTrail/Controllers/TestController.java
+++ src/main/java/com/xDish/PaperTrail/Controllers/TestController.java
@@ -2,6 +2,7 @@ package com.xDish.PaperTrail.Controllers;
 
 
 import com.xDish.PaperTrail.Services.AuthorNameSearch;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/search/{variant}/{query}")
-    public String test(@PathVariable("variant") String variant, @PathVariable("query") String query) throws Exception {
-         return new AuthorNameSearch().parseName(Integer.parseInt(variant),query);
+    public ResponseEntity<String> test(@PathVariable("variant") String variant, @PathVariable("query") String query) throws Exception {
+        String  test = new AuthorNameSearch().parseName(Integer.parseInt(variant),query);
+        return ResponseEntity.ok().body(test);
     }
 }
